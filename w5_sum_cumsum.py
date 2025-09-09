44a663b88f535a0ec80f97098d84ad5f3ba260f6
zachpek <211606199+zachpek@users.noreply.github.com>
2025-09-09 18:30:57 +0800

    w5 lect cumsum

---
+++ w5_sum_cumsum.py
@@ -0,0 +1,13 @@
+# recursive
+
+def s(seq):
+    return seq[-1] + s(seq[:-1]) if seq else 0
+
+def cumsum(seq):
+    return cumsum(seq[:-1]) + (s(seq),) if seq else ()
+
+def cumsum_henry(seq):
+    if len(seq) <= 1:
+        return seq # neat, one less cycle than mine
+    lst = cumsum_henry(seq[:-1])
+    return lst + (lst[-1] + seq[-1],) # nice, cumsum logic here already
