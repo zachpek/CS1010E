7ed6a52496bffc5475f916354ae90a15162c4a09
zachpek <211606199+zachpek@users.noreply.github.com>
2025-09-13 17:07:01 +0800

    abit docstring for task2

---
+++ assignment_2/task2.py
@@ -0,0 +1,11 @@
+def calc_poly(const_seq, var_poly):
+    # Computes the value of a single variable polynomial function given the value of its single variable.
+    # 
+    # Args:
+    #     const_seq
+    #     var_poly
+    # 
+    # Returns:
+    #     float: 
+    
+    pass
