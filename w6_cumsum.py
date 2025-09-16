0a4cba007a4e124d11c54ebaed53d920aa64cd89
zachpek <211606199+zachpek@users.noreply.github.com>
2025-09-16 19:07:35 +0800

    why my code no worky

--- w6_cumsum.py
+++ w6_cumsum.py
@@ -1,4 +1,7 @@
 def cumsum(lst):
+    # lol doesnt work
+    # list lst is not being modified - a new list is being created in the list comprehension
+    # and wrong number of elements (need same element as list)
     return [lst[i] + lst[i-1] for i in range(1,len(lst))]
 
 def cumsum_henry(lst):
