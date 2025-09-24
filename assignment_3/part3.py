a445cc189c25970be6b93cfd6aef991961c584a9
zachpek <211606199+zachpek@users.noreply.github.com>
2025-09-24 18:29:14 +0800

    copied 3c over and wrote docstrings i now hv headache

--- assignment_3/part3.py
+++ assignment_3/part3.py
@@ -70,4 +70,43 @@ def unique_letters(puzzle):
     return tuple_of_letters
 
 def assign(letters, numbers_left, mapping, puzzle):
-    pass
+    # Finds a possible mapping for the puzzle given a sequence of alphabets within the puzzle and a sequence of numbers which we could match the alphabets to.
+    # 
+    # Args:
+    #     letters (tuple[str, ...]): The letters that have yet to be allocated a mapping.
+    #     numbers_left (tuple[int, ...]): The numbers that can be mapped to the letters.
+    #     mapping (str): The current mapping that has been achieved.
+    #     puzzle (tuple[str, ...]): The addition puzzle
+    # 
+    # Returns:
+    #     str or bool: Returns deduced mapping if the above problem is solvable else returns False.
+    
+    if not letters:         # base case 1: possibly found a solution
+        return check(puzzle, mapping)
+    if not numbers_left:    # base case 2: an error
+        return False
+    curr_letter = letters[0]
+    
+    for i in range(len(numbers_left)):
+        # assign a number in numers_left to the current letter
+        # update it to a new map
+        # need to keep the old map for use in the subsequent iterations
+        dgt = numbers_left[i]
+        map_copy = mapping[:]
+        map_copy = mapping[:dgt] + curr_letter + mapping[dgt+1:]
+        
+        # call assign recursively, ensure that the size of the first argument 
+        # (about letters waiting to be matched) has reduced, so the 
+        # recursive call should terminate eventually
+        res = assign(pass,pass,pass,pass) # fill in the arguments here
+        
+        # Check the result from the recursive call
+        if res:
+            return res
+        # at this point here, it fails to find a solution with the
+        # current mapping of number i to curr_letter; so ..
+        # iterate to assign another number to the curr_letter
+
+    # Exiting from the for-loop
+    # going through all available numbers, fail to find a good mapping
+    return False
