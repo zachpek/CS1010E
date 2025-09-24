830efc06d0609d9cd635972065109012ffe3b1f5
zachpek <211606199+zachpek@users.noreply.github.com>
2025-09-24 14:43:17 +0800

    revisions for speed & b & c part1

--- assignment_3/part1.py
+++ assignment_3/part1.py
@@ -8,15 +8,19 @@ def matchResistors(R, n):
     # Returns:
     #     tuple[tuple[int, int], ...]: Tuples of pairs of resistors that sum to n ohms.
 
+    # sorted_list = sorted(R) # see if i fail any private cases
     half = n / 2
     i = 0
     j = -1
+    next_j = -1
     matched = ()
     while R[i] <= half:
         while R[j] > half:
             if R[i] + R[j] == n:
                 matched += ((R[i], R[j]),)
+                next_j = j - 1
+                break
             j -= 1
-        j = -1
+        j = next_j
         i += 1
     return matched
